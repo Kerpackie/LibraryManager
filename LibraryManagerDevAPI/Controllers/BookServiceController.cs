9f83979d00977cfc83b309fd8a05fda112332000
Kerpackie <ckeleo@pm.me>
2024-03-19 22:04:31 +0000

    Update BookServiceController
    Updates the endpoints to test services.

--- LibraryManagerDevAPI/Controllers/BookServiceController.cs
+++ LibraryManagerDevAPI/Controllers/BookServiceController.cs
@@ -27,4 +27,84 @@ public class BookServiceController : ControllerBase
 			? NotFound()
 			: Ok(book);
 	}
+	
+	[HttpGet("{id:int}")]
+	[ProducesResponseType(typeof(Book), StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> Get([FromRoute] int id)
+	{
+		var book = await _bookService.GetBookAsync(id);
+        
+		return book == null
+			? NotFound()
+			: Ok(book);
+	}
+	
+	// Create a book
+	[HttpPost]
+	[ProducesResponseType(typeof(Book), StatusCodes.Status201Created)]
+	public async Task<IActionResult> Create([FromBody] Book book)
+	{
+		await _bookService.CreateBookAsync(book);
+
+		return CreatedAtAction(nameof(Get), new {id = book.Id}, book);
+	}
+	
+	[HttpGet("by-year/{year:int}")]
+	[ProducesResponseType(typeof(List<Book>), StatusCodes.Status200OK)]
+	public async Task<IActionResult> GetAllByYear([FromRoute] int year)
+	{
+		return Ok(await _bookService.GetBooksByPublicationYearAsync(year));
+	}
+	
+	[HttpGet("by-author/{author}")]
+	[ProducesResponseType(typeof(List<Book>), StatusCodes.Status200OK)]
+	public async Task<IActionResult> GetAllByAuthor([FromRoute] string author)
+	{
+		return Ok(await _bookService.GetBooksByAuthorAsync(author));
+	}
+	
+	[HttpGet("by-title/{title}")]
+	[ProducesResponseType(typeof(List<Book>), StatusCodes.Status200OK)]
+	public async Task<IActionResult> GetAllByTitle([FromRoute] string title)
+	{
+		return Ok(await _bookService.GetBooksByTitleAsync(title));
+	}
+	
+	// Get By Publisher
+	[HttpGet("by-publisher/{publisher}")]
+	[ProducesResponseType(typeof(List<Book>), StatusCodes.Status200OK)]
+	public async Task<IActionResult> GetAllByPublisher([FromRoute] string publisher)
+	{
+		return Ok(await _bookService.GetBooksByPublisherAsync(publisher));
+	}
+	
+	[HttpGet("by-genre/{genre}")]
+	[ProducesResponseType(typeof(List<Book>), StatusCodes.Status200OK)]
+	public async Task<IActionResult> GetAllByGenre([FromRoute] string genre)
+	{
+		return Ok(await _bookService.GetBooksByGenreAsync(genre));
+	}
+	
+	[HttpGet("by-page-count/{pageCount:int}")]
+	[ProducesResponseType(typeof(List<Book>), StatusCodes.Status200OK)]
+	public async Task<IActionResult> GetAllByPageCount([FromRoute] int pageCount)
+	{
+		return Ok(await _bookService.GetBooksByNumberOfPagesAsync(pageCount));
+	}
+	
+	[HttpGet("by-page-count/{pageCount:int}")]
+	[ProducesResponseType(typeof(List<Book>), StatusCodes.Status200OK)]
+	public async Task<IActionResult> GetAllByMoreThanPageCount([FromRoute] int pageCount)
+	{
+		return Ok(await _bookService.GetBooksWithMorePagesThanAsync(pageCount));
+	}
+	
+	[HttpGet("by-page-count/{pageCount:int}")]
+	[ProducesResponseType(typeof(List<Book>), StatusCodes.Status200OK)]
+	public async Task<IActionResult> GetAllByLessThanPageCount([FromRoute] int pageCount)
+	{
+		return Ok(await _bookService.GetBooksWithLessPagesThanAsync(pageCount));
+	}
+	
 }
