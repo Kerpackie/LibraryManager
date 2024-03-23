e28a8a9f10d17e1596a093d0bb7f481bfe8e2b94
Kerpackie <ckeleo@pm.me>
2024-03-23 20:03:27 +0000

    Add Cover Service
    Adds the Cover Service
    Adds the ICoverService

---
+++ LibraryManager.Core/Services/CoverService/CoverService.cs
@@ -0,0 +1,45 @@
+﻿using LibraryManager.Core.Data;
+using Microsoft.EntityFrameworkCore;
+using LibraryManager.Core.Models;
+
+namespace LibraryManager.Core.Services.CoverService
+{
+	public class CoverService : ICoverService
+	{
+		private readonly LibraryContext _context;
+
+		public CoverService(LibraryContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<Cover> CreateCoverAsync(Cover cover)
+		{
+			_context.Covers.Add(cover);
+			await _context.SaveChangesAsync();
+			return cover;
+		}
+
+		public async Task<Cover?> GetCoverAsync(int id)
+		{
+			return await _context.Covers.FindAsync(id);
+		}
+
+		public async Task<Cover> UpdateCoverAsync(Cover cover)
+		{
+			_context.Entry(cover).State = EntityState.Modified;
+			await _context.SaveChangesAsync();
+			return cover;
+		}
+
+		public async Task DeleteCoverAsync(int id)
+		{
+			var cover = await _context.Covers.FindAsync(id);
+			if (cover != null)
+			{
+				_context.Covers.Remove(cover);
+				await _context.SaveChangesAsync();
+			}
+		}
+	}
+}
---
+++ LibraryManager.Core/Services/CoverService/ICoverService.cs
@@ -0,0 +1,11 @@
+﻿using LibraryManager.Core.Models;
+
+namespace LibraryManager.Core.Services.CoverService;
+
+public interface ICoverService
+{
+	Task<Cover> CreateCoverAsync(Cover cover);
+	Task<Cover?> GetCoverAsync(int id);
+	Task<Cover> UpdateCoverAsync(Cover cover);
+	Task DeleteCoverAsync(int id);
+}
