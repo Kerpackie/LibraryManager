7ca7a528daa1b0566befcf40c6b41aeb17fab57d
Kerpackie <ckeleo@pm.me>
2024-03-23 20:05:51 +0000

    Add CoverMapping
    Adds CoverMapping to correctly map the database.

---
+++ LibraryManager.Core/Data/EntityMapping/CoverMapping.cs
@@ -0,0 +1,26 @@
+﻿using LibraryManager.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LibraryManager.Core.Data.EntityMapping;
+
+public class CoverMapping : IEntityTypeConfiguration<Cover>
+{
+	public void Configure(EntityTypeBuilder<Cover> builder)
+	{
+		builder.Property(c => c.Large)
+			.IsRequired()
+            .HasColumnType("varchar")
+			.HasMaxLength(256);
+		
+		builder.Property(c => c.Medium)
+			.IsRequired()
+			.HasColumnType("varchar")
+			.HasMaxLength(256);
+		
+		builder.Property(c => c.Small)
+			.IsRequired()
+			.HasColumnType("varchar")
+			.HasMaxLength(256);
+	}
+}
