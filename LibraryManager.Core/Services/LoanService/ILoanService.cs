683241310bb66d690711aa800d71358e9f6f4425
Kerpackie <ckeleo@pm.me>
2024-03-26 12:25:44 +0000

    Add Loan Service

---
+++ LibraryManager.Core/Services/LoanService/ILoanService.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManager.Core.Models;
+
+namespace LibraryManager.Core.Services.LoanService;
+
+public interface ILoanService
+{
+	Task<ServiceResponse<Loan>> CreateLoanAsync(Loan newLoan);
+	Task<ServiceResponse<Loan>> GetLoanAsync(int id);
+	Task<ServiceResponse<Loan>> GetLoanByBookIdAsync(int bookId);
+	Task<ServiceResponse<IEnumerable<Loan>>> GetAllLoansAsync();
+	Task<ServiceResponse<IEnumerable<Loan>>> GetLoansByBorrowerAsync(string borrower);
+	Task<ServiceResponse<Loan>> ReturnLoanAsync(int id);
+	Task<ServiceResponse<Loan>> UpdateLoanAsync(Loan updatedLoan);
+	Task<ServiceResponse<Loan>> DeleteLoanAsync(int id);
+}
